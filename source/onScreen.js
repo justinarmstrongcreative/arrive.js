8ac7795a7b7a749839c6f18cef6f13b1ec5c9f8c
Justin Armstrong <jarmstrong@planitagency.com>
2018-08-21 17:34:54 -0400

    Adds duration and delay

--- source/onScreen.js
+++ source/onScreen.js
@@ -25,13 +25,34 @@ function onScreen(element) {
             offset_top = this_offset.top, // Element offset top
             offset_bot = offset_top + this_height, // Element offset bottom
 
-            percent = 0;
+            percent = 0,
+            delay = 0.5;
 
-        // if has percentage
+        // percentage
         if(this_classes.indexOf('on-screen_percent') > -1) {
-            for(var i = 0; i < this_class_array.length; i++) {
-                if(this_class_array[i].indexOf('on-screen_percent') > -1) {
-                    percent = parseInt(this_class_array[i].split('--').pop().trim()) / 100;
+            for(var p = 0; p < this_class_array.length; p++) {
+                if(this_class_array[p].indexOf('on-screen_percent') > -1) {
+                    percent = parseInt(this_class_array[p].split('--').pop().trim()) / 100;
+                }
+            }
+        }
+
+        // duration
+        if(this_classes.indexOf('on-screen_duration') > -1) {
+            for(var du = 0; du < this_class_array.length; du++) {
+                if(this_class_array[du].indexOf('on-screen_duration') > -1) {
+                    duration = parseInt(this_class_array[du].split('--').pop().trim()) / 1000;
+                    $this.css('transition-duration', duration+'s');
+                }
+            }
+        }
+
+        // delay
+        if(this_classes.indexOf('on-screen_delay') > -1) {
+            for(var de = 0; de < this_class_array.length; de++) {
+                if(this_class_array[de].indexOf('on-screen_delay') > -1) {
+                    delay = parseInt(this_class_array[de].split('--').pop().trim()) / 1000;
+                    $this.css('transition-delay', delay+'s');
                 }
             }
         }
